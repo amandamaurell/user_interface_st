1e010cbb85459e82832574ca2f73a13765a3b50c
Amanda Maurell <amanda.maurell@gmail.com>
2022-11-24 23:13:10 +0100

    push 2

--- app/pages/page_01.py
+++ app/pages/page_01.py
@@ -2,9 +2,8 @@ import pandas as pd
 import streamlit as st
 
 def page1():
-    st.markdown("""
-                #This is the content of my page 01
-                """)
+    if st.button('click ;)'):
+        st.image('raw_data/wagon.png')
     
 page1()
 #the directory has to be named pages. Streamlit identifies it. 
